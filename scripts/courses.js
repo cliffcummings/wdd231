2f4cad1eddb734e03431cdd028efa248cc6d2539
Cliff Cummings <cliffc@sunburst-design.com>
2024-11-19 09:52:04 -0700

    CSS and JS had comments at the end of the code

--- scripts/courses.js
+++ scripts/courses.js
@@ -192,29 +192,3 @@ function createCoursesCard(filteredcourses) {
         doc.querySelector(".container").appendChild(coursename);
 	})
 }
-
-//----------------------------------------------------------------------------
-// function mobileAdjust() {
-//     // Get width and height of the window excluding scrollbars
-//     let w = Number(doc.documentElement.clientWidth);
-//     // console.log(`w = ${w}`);
-
-//     if (w > 700) {
-//         doc.getElementById("image-src").innerHTML = `<img src="./images/photo_cliff_374_480.png" 
-//                                              alt="Cliff Cummings photograph" height="480" align="left">`;
-//         // console.log("Large size!");
-//     } else {
-//         doc.getElementById("image-src").innerHTML = `<img src="./images/photo_cliff_374_480.png" 
-//         alt="Cliff Cummings photograph" height="240" align="left">`;
-//         // doc.getElementById("image-src").innerHTML = `<img src="./images/photo_cliff_187_240.png" 
-//         //                                      alt="Cliff Cummings photograph" height="240" align="left">`;
-//         // console.log("Small size!");
-//     };
-// }
-
-// // Attaching the event listener function to window's resize event
-// window.addEventListener("resize", mobileAdjust);
-
-// // Calling the function for the first time
-// mobileAdjust();
-//----------------------------------------------------------------------------
