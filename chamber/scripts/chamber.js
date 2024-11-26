552522a11490f7073399d1ef8598e08e40c13f7b
Cliff Cummings <cliffc@sunburst-design.com>
2024-11-26 14:21:09 -0700

    Some business cards partially working

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -7,3 +7,57 @@ hamburgerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburgerElement.classList.toggle('open');
 });
+
+const url = 'data/members.json';
+const cards = document.querySelector('#cards');
+  
+console.log("Going to GET business data");
+getBusinessData();
+
+async function getBusinessData() {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        console.log("RESPONSE data");
+        console.table(data.companies);
+        // const companyList = JSON.parse(data);
+        // const companyList = Array.from(data);
+        // console.table(companyList); // temporary testing of data.company (array) response
+
+        displayBusinesses(data.companies);
+    }
+}   
+
+const displayBusinesses = (companies) => {
+    console.log("Going to DISPLAY business data");
+    console.table(companies);
+
+	// cards defined at top of this file
+	cards.innerHTML = "";
+    
+    companies.forEach((company) => {
+        console.table(company);
+      // Create elements to add to the div.cards element
+      let card = doc.createElement('section');
+      let h2 = doc.createElement('h2'); // fill in the blank
+      let phone = doc.createElement('p');
+      let logo = doc.createElement('img');
+  
+      // Build the h2 content out to show company name
+      h2.innerHTML = `${company.name}`;
+      phone.innerHTML = `${company.phoneNumber}`
+      // Build the logo image 
+      logo.setAttribute('src', company.imageurl);
+      logo.setAttribute('alt', `logo of ${company.name}`); // fill in the blank
+      logo.setAttribute('loading', 'lazy');
+      logo.setAttribute('width', '140');
+      logo.setAttribute('height', 'auto');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(h2);
+      card.appendChild(phone);  
+      card.appendChild(logo);
+  
+      cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+  }
