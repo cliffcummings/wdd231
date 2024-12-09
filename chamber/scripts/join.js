17b77c546794a1a7c74620dc6992e3257ac4737c
Cliff Cummings <cliffc@sunburst-design.com>
2024-12-09 11:43:36 -0700

    Added thanks.html and updated join.js

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -5,41 +5,41 @@ console.log(currentUrl);
 // Divide the url into two halves
 const everything=currentUrl.split('?');
 // console.log(everything);
-let formData = everything;
+
+// Grab just the second half [1]
+// Break the form name-value pairs into an array
+let formData = everything[1].split("&");
 console.log(formData);
 
-// Grab just the second half
-// let formData = everything[1].split('&');
-// console.log(formData);
+const thankyou = document.querySelector("#thankyou");
 
-// Break the form name-value pairs into an array
-// formData = formData.split('&');
-// console.log(formData);
-// function show(cup) {
-//     // console.log(cup);
-//     formData.forEach((element) => {
-//         // console.log(element);
-//         if (element.startsWith(cup)) {
-//             // console.log("Found a Match");
-//             result=element.split('=')[1].replace("%40","@");
-//             // === Long version === //
-//             // result=element.split('=');
-//             // result=result[1];
-//             // result=result.replace("%40","@");
-//             // console.log(result);
-//         }
-//     });
-//     return (result);
-// }
+thankyou.innerHTML = `
+    <h1>YOUR APPLICATION HAS BEEN RECEIVED</h1>
+    <p>Thank you for applying for Membership Level: ${show("level")}</p>
+    <p>Name: ${show("firstname")} ${show("lastname")}</p>
+    <p>Your Phone: ${show("mobile")}</p>
+    <p>Your Email: <a href="${show("email")}"> ${show("email")}</a></p>
+    <p>Business/Organization: ${show("orgname")}</p>
+    `
+// <p>${show("timestamp")}</p>
 
-// const showInfo = document.querySelector('#results');
-// // showInfo.innerHTML = formData[0] + formData[1];
-// // showInfo.innerHTML = show("email");
-// showInfo.innerHTML = `
-// <p>Appointment for ${show("first")} ${show("last")}</p>
-// <p>Proxy ${show('ordinance')} on ${show('fecha')} in the ${show('location')} Temple</p>
-// <p>Your Phone: ${show('phone')}</p>
-// <p>Your Email: <a href=${show('email')}>${show('email')}</a></p>
-// `;
+function show(info) {
+    // console.log(info);
+    formData.forEach((element) => {
+        // console.log(element);
+        if (element.startsWith(info)) {
+            // console.log("Found a Match");
+            result=element.split('=')[1].replace("%40","@");
+            // === Long version === //
+            // result=element.split('=');
+            // result=result[1];
+            // result=result.replace("%40","@");
+            // console.log(result);
+            // result = item.split("=")[1].replace("%40", "@").replaceAll("%3A", ":").replaceAll("+", " ").replaceAll("%28", "(").replaceAll("%29", ")");
+        }
+    });
+    return (result);
+}
 
-// show("phone");
+
+show("firstname");
