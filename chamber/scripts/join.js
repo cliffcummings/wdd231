2cd1949d395d27887c6085bd3f74350be5b69ad3
Cliff Cummings <cliffc@sunburst-design.com>
2024-12-11 15:00:13 -0700

    Trying to debug Close Button

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,14 +1,78 @@
-// const form = document.querySelector("#form");
+const url="../data/memberLevels.json";
 
-const openButton = document.querySelector("#openButton");
+// const openButton = document.querySelector("#openButton");
 const dialogBox = document.querySelector("#dialogBox");
 const closeButton = document.querySelector("#closeButton");
 
-openButton.addEventListener("click", () => {
-    console.log("openButton clicked");
+const memberLevel = document.querySelector("#memberlevels");
+
+// openButton.addEventListener("click", () => {
+//     console.log("openButton clicked");
+//     dialogBox.showModal();
+// });
+
+// closeButton.addEventListener("click", () => {
+//     dialogBox.close();
+// });
+
+async function apiFetch(url) {
+// First:  await fetch(weatherUrl);      for the web page to responsd
+// Second: await response.json(); for the web page to send us the requested data
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);    // testing only
+            displayMemberInfo(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+function displayMemberInfo(data) {
+    console.log(data);
+    // const memberLevel = document.createElement("#memberlevels");
+
+    data.memberLevels.forEach(item => {
+        const levelDiv = document.createElement("div");
+        levelDiv.classList.add("card");
+
+        levelDiv.innerHTML = `
+        <h4>${item.name}</h4>
+        <button>More Level Infomation</button>
+        `;
+
+        levelDiv.addEventListener("click", () => {
+            console.log(item);
+            displayLevelDetails(item);
+        });
+
+        memberLevel.appendChild(levelDiv);
+    });
+};
+
+
+function displayLevelDetails(item) {
+    console.log("This displayLevelsData One!");
+    console.log(item);
+    dialogBox.innerHTML = `
+    <h4>${item.name}</h4>
+    <button id="closeButton">Close</button>
+    `;
     dialogBox.showModal();
-});
+    // dialogBox.scrollTop = 0;
+    // closeButton.addEventListener("click", () => {
+    //     console.log("Close button selected");
+    //     dialogBox.close();
+    // });
+};
+
+apiFetch(url);
 
 closeButton.addEventListener("click", () => {
+    console.log("Close button selected")
     dialogBox.close();
 });
