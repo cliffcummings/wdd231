af6db97224d26cc913d87e15e3861675863b0e59
Cliff Cummings <cliffc@sunburst-design.com>
2024-12-15 20:37:00 -0700

    Save some initial sunburst design web page files

--- sunburst-design/scripts/gridButton.js
+++ sunburst-design/scripts/gridButton.js
@@ -1,11 +1,11 @@
-gridButton.addEventListener('click', () => {
-    console.log("gridButton clicked");
-    gridChoice = "grid";
-    cards.setAttribute('class', gridChoice);
-});
+// gridButton.addEventListener('click', () => {
+//     console.log("gridButton clicked");
+//     gridChoice = "grid";
+//     cards.setAttribute('class', gridChoice);
+// });
 
-listButton.addEventListener('click', () => {
-    console.log("listButton clicked");
-    gridChoice = "list";
-    cards.setAttribute('class', gridChoice);
-});
+// listButton.addEventListener('click', () => {
+//     console.log("listButton clicked");
+//     gridChoice = "list";
+//     cards.setAttribute('class', gridChoice);
+// });
