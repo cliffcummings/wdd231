ffd57ff13e2f52204cdfebd40df8e12647852a83
Cliff Cummings <cliffc@sunburst-design.com>
2024-12-17 22:54:41 -0700

    animateme changed from id to class

--- sunburst-design/scripts/menu.js
+++ sunburst-design/scripts/menu.js
@@ -1,6 +1,6 @@
 const hamburgerElement = document.querySelector('#myButton');
 // const navElement = document.querySelector('.menuLinks'); // TODO
-const navElement = document.querySelector('#animateme'); // TODO
+const navElement = document.querySelector('.animateme'); // TODO
 
 hamburgerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
